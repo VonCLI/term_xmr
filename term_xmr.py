2733dc127203399466cbbc542742a61e1acae2fa
VonCLI <4ltm1nd@protonmail.com>
2022-04-21 23:32:38 +0200

    Revert "adding major coins and portfolio possibilities"
    This reverts commit 33713cd005cc414b50a3b556dda9f90f95302c36.

--- term_xmr.py
+++ term_xmr.py
@@ -3,14 +3,12 @@
 
 import time
 import os
-
-from  pycoingecko import CoinGeckoAPI
 from termcolor import cprint
+from  pycoingecko import CoinGeckoAPI
 from datetime import datetime
 
 
 cg = CoinGeckoAPI()
-pf = float(input("Amount of $XMR: "))
 
 def monero_price():
 
@@ -50,52 +48,12 @@ def monero_price():
     Low/24h: {low} us$
     """)
 
-def major():
-    btc = cg.get_coins_markets(ids='bitcoin', vs_currency='usd')
-    eth = cg.get_coins_markets(ids='ethereum', vs_currency='usd')
-
-    for i in btc:
-        bitcoin = i['name']
-        btcprice = i['current_price']
-    for i in eth:
-        ethereum = i['name']
-        ethprice = i['current_price']
-        
-    print(f"""    
-    {bitcoin}:{btcprice} us$
-    {ethereum}: {ethprice} us$
-    """)
-
-
-def portfolio():
-    xmrus = cg.get_coins_markets(ids='monero', vs_currency='usd',include_market_cap='True')
-    xmrbtc = cg.get_coins_markets(ids='monero', vs_currency='btc')
-    xmreur = cg.get_coins_markets(ids='monero', vs_currency='eur')
-    
-    for i in xmrus:
-        price_us = i['current_price']
-    for i in xmrbtc:
-        price_btc = i['current_price']
-    for i in xmreur:
-        price_eur = i['current_price']
-    
-    usd = pf * float(price_us)
-    btc = pf * float(price_btc)
-    eur = pf * float(price_eur)
-
-    print(f"""
-    Value of your portfolio: {usd:.2f} $us | {btc:.8f} ₿ | {eur:.2f} €
-    """)
-
-
 def clear():
     os.system('clear')
 
 while True:
     try:
         monero_price()
-        major()
-        portfolio()
         time.sleep(30)    #results updated each 30 seconds.
         clear()
     except KeyboardInterrupt:
